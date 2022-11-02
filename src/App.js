2c3a1a36b3faef3cde921c8f79f414e7821bb492
Johnny Estrada <johnnye562@yahoo.com>
2022-11-02 13:40:45 -0700

    Create card component and styles

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import data from './data';
 import './App.css';
 
 function App() {
+  const cards = data.map(item => {
+    return (
+        <Card 
+            img={item.imageUrl}
+            title={item.title} 
+            location={item.location}
+            link={item.googleMapsUrl}
+            startDate={item.startDate}
+            endDate={item.endDate} 
+            description={item.description} 
+        />
+    )
+})
+
   return (
     <div className="container">
       <Navbar />
-      <Card />
+      {cards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 function Card(props) {
-    console.log(props)
+    // Logic
+    
+    // Card Component
     return (
-        <section className='card'>
-            <img className='card__img' src={props.imageUrl} alt='Mt. Fuji in Japan.' />
+        <section className="card">
+            <img className="card__img" src={props.img} alt="Some person standing." />
+            <div className="card-content">
+                <div className="location">
+                    <img src="/images/location-icon.svg" width="7px" height="10px" alt="Some person standing." />
+                    <h3 className="location-title">{props.location}</h3>
+                    <p className="location-text"><a href={props.link}>View on Google Maps</a></p>
+                </div>
+                <h2>{props.title}</h2>
+                <h3>{props.startDate} - {props.endDate}</h3>
+                <p>{props.description}</p>
+            </div>
+            <div className="line-break"></div>
         </section>
     )
 }
